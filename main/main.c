a2d059857da903b4bd4c98157e2c0b857ed157ae
Renze Nicolai <renze@rnplus.nl>
2022-07-12 00:04:27 +0200

    HDMI stuff!

--- main/main.c
+++ main/main.c
@@ -1,84 +1,241 @@
-/*
- * This example code is in the Public Domain (or CC0 licensed, at your option.)
- * Unless required by applicable law or agreed to in writing, this
- * software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
- * CONDITIONS OF ANY KIND, either express or implied.
- */
-
+#include <string.h>
+#include <esp_err.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <freertos/task.h>
 #include "main.h"
+#include "fpga_util.h"
+#include "ws2812.h"
 
-static pax_buf_t buf;
-xQueueHandle buttonQueue;
+extern const uint8_t bitstream_bin_start[] asm("_binary_bitstream_bin_start");
+extern const uint8_t bitstream_bin_end[] asm("_binary_bitstream_bin_end");
 
-static const char *TAG = "mch2022-demo-app";
+extern const uint8_t animation_frame_1_png_start[]  asm("_binary_animation_frame_1_png_start");
+extern const uint8_t animation_frame_2_png_start[]  asm("_binary_animation_frame_2_png_start");
+extern const uint8_t animation_frame_3_png_start[]  asm("_binary_animation_frame_3_png_start");
+extern const uint8_t animation_frame_4_png_start[]  asm("_binary_animation_frame_4_png_start");
+extern const uint8_t animation_frame_5_png_start[]  asm("_binary_animation_frame_5_png_start");
+extern const uint8_t animation_frame_6_png_start[]  asm("_binary_animation_frame_6_png_start");
+extern const uint8_t animation_frame_7_png_start[]  asm("_binary_animation_frame_7_png_start");
+extern const uint8_t animation_frame_8_png_start[]  asm("_binary_animation_frame_8_png_start");
+extern const uint8_t animation_frame_9_png_start[]  asm("_binary_animation_frame_9_png_start");
+extern const uint8_t animation_frame_10_png_start[] asm("_binary_animation_frame_10_png_start");
+extern const uint8_t animation_frame_11_png_start[] asm("_binary_animation_frame_11_png_start");
+extern const uint8_t animation_frame_12_png_start[] asm("_binary_animation_frame_12_png_start");
+extern const uint8_t animation_frame_13_png_start[] asm("_binary_animation_frame_13_png_start");
+extern const uint8_t animation_frame_14_png_start[] asm("_binary_animation_frame_14_png_start");
+extern const uint8_t animation_frame_15_png_start[] asm("_binary_animation_frame_15_png_start");
+extern const uint8_t animation_frame_16_png_start[] asm("_binary_animation_frame_16_png_start");
+extern const uint8_t animation_frame_17_png_start[] asm("_binary_animation_frame_17_png_start");
+extern const uint8_t animation_frame_18_png_start[] asm("_binary_animation_frame_18_png_start");
+extern const uint8_t animation_frame_19_png_start[] asm("_binary_animation_frame_19_png_start");
+extern const uint8_t animation_frame_20_png_start[] asm("_binary_animation_frame_20_png_start");
+extern const uint8_t animation_frame_21_png_start[] asm("_binary_animation_frame_21_png_start");
+extern const uint8_t animation_frame_22_png_start[] asm("_binary_animation_frame_22_png_start");
+extern const uint8_t animation_frame_23_png_start[] asm("_binary_animation_frame_23_png_start");
+extern const uint8_t animation_frame_24_png_start[] asm("_binary_animation_frame_24_png_start");
+extern const uint8_t animation_frame_25_png_start[] asm("_binary_animation_frame_25_png_start");
+extern const uint8_t animation_frame_26_png_start[] asm("_binary_animation_frame_26_png_start");
+extern const uint8_t animation_frame_27_png_start[] asm("_binary_animation_frame_27_png_start");
+extern const uint8_t animation_frame_28_png_start[] asm("_binary_animation_frame_28_png_start");
 
-// Updates the screen with the last drawing.
-void disp_flush() {
-    ili9341_write(get_ili9341(), buf.buf);
+extern const uint8_t animation_frame_1_png_end[]  asm("_binary_animation_frame_1_png_end");
+extern const uint8_t animation_frame_2_png_end[]  asm("_binary_animation_frame_2_png_end");
+extern const uint8_t animation_frame_3_png_end[]  asm("_binary_animation_frame_3_png_end");
+extern const uint8_t animation_frame_4_png_end[]  asm("_binary_animation_frame_4_png_end");
+extern const uint8_t animation_frame_5_png_end[]  asm("_binary_animation_frame_5_png_end");
+extern const uint8_t animation_frame_6_png_end[]  asm("_binary_animation_frame_6_png_end");
+extern const uint8_t animation_frame_7_png_end[]  asm("_binary_animation_frame_7_png_end");
+extern const uint8_t animation_frame_8_png_end[]  asm("_binary_animation_frame_8_png_end");
+extern const uint8_t animation_frame_9_png_end[]  asm("_binary_animation_frame_9_png_end");
+extern const uint8_t animation_frame_10_png_end[] asm("_binary_animation_frame_10_png_end");
+extern const uint8_t animation_frame_11_png_end[] asm("_binary_animation_frame_11_png_end");
+extern const uint8_t animation_frame_12_png_end[] asm("_binary_animation_frame_12_png_end");
+extern const uint8_t animation_frame_13_png_end[] asm("_binary_animation_frame_13_png_end");
+extern const uint8_t animation_frame_14_png_end[] asm("_binary_animation_frame_14_png_end");
+extern const uint8_t animation_frame_15_png_end[] asm("_binary_animation_frame_15_png_end");
+extern const uint8_t animation_frame_16_png_end[] asm("_binary_animation_frame_16_png_end");
+extern const uint8_t animation_frame_17_png_end[] asm("_binary_animation_frame_17_png_end");
+extern const uint8_t animation_frame_18_png_end[] asm("_binary_animation_frame_18_png_end");
+extern const uint8_t animation_frame_19_png_end[] asm("_binary_animation_frame_19_png_end");
+extern const uint8_t animation_frame_20_png_end[] asm("_binary_animation_frame_20_png_end");
+extern const uint8_t animation_frame_21_png_end[] asm("_binary_animation_frame_21_png_end");
+extern const uint8_t animation_frame_22_png_end[] asm("_binary_animation_frame_22_png_end");
+extern const uint8_t animation_frame_23_png_end[] asm("_binary_animation_frame_23_png_end");
+extern const uint8_t animation_frame_24_png_end[] asm("_binary_animation_frame_24_png_end");
+extern const uint8_t animation_frame_25_png_end[] asm("_binary_animation_frame_25_png_end");
+extern const uint8_t animation_frame_26_png_end[] asm("_binary_animation_frame_26_png_end");
+extern const uint8_t animation_frame_27_png_end[] asm("_binary_animation_frame_27_png_end");
+extern const uint8_t animation_frame_28_png_end[] asm("_binary_animation_frame_28_png_end");
+
+const void *anim_start_regions[] = {
+    animation_frame_1_png_start,
+    animation_frame_2_png_start,
+    animation_frame_3_png_start,
+    animation_frame_4_png_start,
+    animation_frame_5_png_start,
+    animation_frame_6_png_start,
+    animation_frame_7_png_start,
+    animation_frame_8_png_start,
+    animation_frame_9_png_start,
+    animation_frame_10_png_start,
+    animation_frame_11_png_start,
+    animation_frame_12_png_start,
+    animation_frame_13_png_start,
+    animation_frame_14_png_start,
+    animation_frame_15_png_start,
+    animation_frame_16_png_start,
+    animation_frame_17_png_start,
+    animation_frame_18_png_start,
+    animation_frame_19_png_start,
+    animation_frame_20_png_start,
+    animation_frame_21_png_start,
+    animation_frame_22_png_start,
+    animation_frame_23_png_start,
+    animation_frame_24_png_start,
+    animation_frame_25_png_start,
+    animation_frame_26_png_start,
+    animation_frame_27_png_start,
+    animation_frame_28_png_start,
+};
+
+const void *anim_end_regions[] = {
+    animation_frame_1_png_end,
+    animation_frame_2_png_end,
+    animation_frame_3_png_end,
+    animation_frame_4_png_end,
+    animation_frame_5_png_end,
+    animation_frame_6_png_end,
+    animation_frame_7_png_end,
+    animation_frame_8_png_end,
+    animation_frame_9_png_end,
+    animation_frame_10_png_end,
+    animation_frame_11_png_end,
+    animation_frame_12_png_end,
+    animation_frame_13_png_end,
+    animation_frame_14_png_end,
+    animation_frame_15_png_end,
+    animation_frame_16_png_end,
+    animation_frame_17_png_end,
+    animation_frame_18_png_end,
+    animation_frame_19_png_end,
+    animation_frame_20_png_end,
+    animation_frame_21_png_end,
+    animation_frame_22_png_end,
+    animation_frame_23_png_end,
+    animation_frame_24_png_end,
+    animation_frame_25_png_end,
+    animation_frame_26_png_end,
+    animation_frame_27_png_end,
+    animation_frame_28_png_end,
+};
+
+const size_t num_anim  = sizeof(anim_start_regions)  / sizeof(const void *);
+
+static pax_buf_t pax_buffer;
+xQueueHandle button_queue;
+
+static uint8_t *hdmi_spi_buf = NULL;
+static uint32_t fpga_base = 0;
+
+void lcd_flush() {
+    ili9341_write(get_ili9341(), pax_buffer.buf);
 }
 
-// Exits the app, returning to the launcher.
+void hdmi_flush() {
+    ICE40* ice40 = get_ice40();
+    hdmi_spi_buf[0] = 0xe0; // Write to memory command
+    for (uint8_t index = 0; index < 40; index++) {
+        memcpy(&hdmi_spi_buf[1], &(((uint8_t*) pax_buffer.buf)[3840 * index]), 3840);
+        ice40_send_turbo(ice40, hdmi_spi_buf, 3841);
+    }
+    struct fpga_wb_cmdbuf *cq = fpga_wb_alloc(1);
+    fpga_wb_queue_write(cq, 1, 4, fpga_base);
+    fpga_wb_exec(cq, get_ice40());
+    fpga_base += 320 * 240;
+}
+
+
 void exit_to_launcher() {
     REG_WRITE(RTC_CNTL_STORE0_REG, 0);
     esp_restart();
 }
 
+void display_logo(const void *start, const void *end) {
+    size_t len = (size_t) end - (size_t) start;
+    pax_decode_png_buf(&pax_buffer, (void *) start, len, pax_buffer.type, CODEC_FLAG_EXISTING);
+}
+
 void app_main() {
-    // Init the screen, the I2C and the SPI busses.
     bsp_init();
-    // Init the RP2040 (responsible for buttons among other important things).
     bsp_rp2040_init();
-    // This queue is used to await button presses.
-    buttonQueue = get_rp2040()->queue;
-    
-    // Init graphics for the screen.
-    pax_buf_init(&buf, NULL, 320, 240, PAX_BUF_16_565RGB);
-    
-    // Init NVS.
+    bsp_ice40_init();
+    button_queue = get_rp2040()->queue;
+    pax_buf_init(&pax_buffer, NULL, 320, 240, PAX_BUF_16_565RGB);
     nvs_flash_init();
-    
-    // Init (but not connect to) WiFi.
     wifi_init();
     
-    while (1) {
-        // Pick a random background color.
-        int hue = esp_random() & 255;
-        pax_col_t col = pax_col_hsv(hue, 255 /*saturation*/, 255 /*brighness*/);
-        
-        // Show some random color hello world.
-        // Draw the background with aforementioned random color.
-        pax_background(&buf, col);
-        // This text is shown on screen.
-        char             *text = "Hello, World!";
-        // Pick the font to draw in (this is the only one that looks nice this big).
-        const pax_font_t *font = pax_font_saira_condensed;
-        // Determine how large the text is so it can be centered.
-        pax_vec1_t        dims = pax_text_size(font, font->default_size, text);
-        // Use info to draw the centered text.
-        pax_draw_text(
-            // Buffer to draw to.
-            &buf, 0xff000000,
-            // Font and size to use.
-            font, font->default_size,
-            // Position (top left corner) of the app.
-            (buf.width  - dims.x) / 2.0,
-            (buf.height - dims.y) / 2.0,
-            // The text to be shown.
-            text
-        );
-        // Draws the entire graphics buffer to the screen.
-        disp_flush();
-        
-        // Await any button press and do another cycle.
-        // Structure used to receive data.
+    hdmi_spi_buf = malloc(3841);
+
+    esp_err_t res = ice40_load_bitstream(get_ice40(), bitstream_bin_start, bitstream_bin_end - bitstream_bin_start);
+    if (res != ESP_OK) {
+        pax_background(&pax_buffer, 0xFF0000);
+        pax_draw_text(&pax_buffer, 0xFFFFFFFF, pax_font_saira_condensed, 18, 10, 10, "Failed to load bitstream");
+        ili9341_write(get_ili9341(), pax_buffer.buf);
         rp2040_input_message_t message;
-        // Await forever (because of portMAX_DELAY), a button press.
-        xQueueReceive(buttonQueue, &message, portMAX_DELAY);
-        
-        // Is the home button currently pressed?
-        if (message.input == RP2040_INPUT_BUTTON_HOME && message.state) {
-            // If home is pressed, exit to launcher.
-            exit_to_launcher();
+        xQueueReceive(button_queue, &message, portMAX_DELAY);
+        exit_to_launcher();
+    }
+
+    uint32_t dummy;
+    struct fpga_wb_cmdbuf *cq = fpga_wb_alloc(16);
+
+    fpga_wb_queue_write(cq, 1, 0x00, 0);	        // Disable HDMI DMA
+    fpga_wb_queue_write(cq, 2, 0x00, 0);	        // Disable SPI DMA
+
+    fpga_wb_queue_write(cq, 0, 0x00, 0x00000004);   // Reset   manual control
+    fpga_wb_queue_write(cq, 0, 0x00, 0x00000002);   // Request manual control
+    fpga_wb_queue_write(cq, 0, 0x40, 0x35000000);   // Send SPI command 0x35 (enable QPI)
+    fpga_wb_queue_read (cq, 0, 0x0c, &dummy);       // Discard RF
+    fpga_wb_queue_write(cq, 0, 0x00, 0x00000004);   // Release manual control
+
+    fpga_wb_queue_write(cq, 1, 0x04, fpga_base);    // Set HDMI base address
+    fpga_wb_queue_write(cq, 1, 0x00, 1);	        // Enable HDMI DMA
+
+    fpga_wb_queue_write(cq, 2, 0x04, fpga_base);    // Set SPI base address
+    fpga_wb_queue_write(cq, 2, 0x00, 1);	        // Enable SPI DMA
+
+    fpga_wb_exec(cq, get_ice40());
+    
+    printf("Ready.\r\n");
+    
+    // Power on LED region.
+    uint8_t leds[15];
+    memset(&leds, 0, sizeof(leds));
+    gpio_set_direction(GPIO_SD_PWR, GPIO_MODE_OUTPUT);
+    gpio_set_level(GPIO_SD_PWR, true);
+    // Init LEDs to off.
+    ws2812_init(GPIO_LED_DATA);
+    ws2812_send_data(leds, sizeof(leds));
+    
+    
+    const pax_font_t *font = pax_font_saira_condensed;
+    
+    while (1) {
+        for (int i = 0; i < num_anim; i++) {
+            display_logo(anim_start_regions[i], anim_end_regions[i]);
+            lcd_flush();
+            hdmi_flush();
+        }
+        for (int i = 0; i < num_anim; i++) {
+            display_logo(anim_start_regions[i], anim_end_regions[i]);
+            pax_draw_text(&pax_buffer, 0xFFFFFFFF, font, font->default_size, 10, 10, "LCD OUTPUT");
+            lcd_flush();
+            display_logo(anim_start_regions[i], anim_end_regions[i]);
+            pax_draw_text(&pax_buffer, 0xFFFFFFFF, font, font->default_size, 10, 10, "HDMI OUTPUT");
+            hdmi_flush();
         }
     }
 }
